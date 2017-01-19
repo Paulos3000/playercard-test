4bf6e44bf224d558e4d740dcea8f58d347f61191
Paul Child <pauljchild@gmail.com>
2017-01-19 21:20:40 +0000

    add NotFoundPage

---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundPage = () => (
+   <div className='jumbotron'>
+      <div className='container'>
+         <h1 className='centered-text'>Page Not Found</h1>
+      </div>
+   </div>
+)
+
+export default NotFoundPage
