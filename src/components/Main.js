e223e983ba92f6ba07fa17bfb1b560d26eda6f59
Paul Child <pauljchild@gmail.com>
2017-01-19 21:25:00 +0000

    update main

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,7 @@ export default class Main extends Component {
    render() {
       return (
          <div>
+            <img src='driulis-gonzalez.jpg' />
             <PlayerCard />
          </div>
       )
