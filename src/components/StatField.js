4dfeb45509765f830d1fc63e94ff803321ba8cc8
Paul Child <pauljchild@gmail.com>
2017-01-25 17:20:57 +0000

    remove redundant code

--- src/components/StatField.js
+++ src/components/StatField.js
@@ -2,12 +2,6 @@ import React from 'react'
 
 const StatField = ({ attribute, value }) => {
 
-   const attributeMap = {
-      D: 'Defender',
-      M: 'Midfielder',
-      F: 'Forward',
-   }
-
    return (
    <div className='stat-field'>
       <p>
