fbb75b18a7ba0f798e656ecdc630f7743fb86e62
Paul Child <pauljchild@gmail.com>
2017-01-25 17:22:52 +0000

    remove redundant code

--- src/components/StatField.js
+++ src/components/StatField.js
@@ -3,12 +3,12 @@ import React from 'react'
 const StatField = ({ attribute, value }) => {
 
    return (
-   <div className='stat-field'>
-      <p>
-         <span>{attribute}</span>
-         <span>{value}</span>
-      </p>
-   </div>
+      <div className='stat-field'>
+         <p>
+            <span>{attribute}</span>
+            <span>{value}</span>
+         </p>
+      </div>
    )
 }
 
